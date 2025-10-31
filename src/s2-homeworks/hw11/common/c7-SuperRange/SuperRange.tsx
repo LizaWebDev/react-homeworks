7c4d28e1ce42887577b7d60d03c3380fa6a0d6b0
LizaWebDev <e.kuntsevich03@gmail.com>
2025-10-31 14:39:04 +0300

    Homework 11

--- src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
+++ src/s2-homeworks/hw11/common/c7-SuperRange/SuperRange.tsx
@@ -4,32 +4,32 @@ import {Slider, SliderProps} from '@mui/material'
 const SuperRange: React.FC<SliderProps> = (props) => {
     return (
         <Slider
-            sx={{ // стили для слайдера // пишет студент
-                color: '#00CC22',
-                height: 5,
+            sx={{
                 width: 300,
-                '& .MuiSlider-track': {
-                    border: 'none',
-                    backgroundColor: '#00CC22',
-                },
-                '& .MuiSlider-rail': {
-                    opacity: 0.5,
-                    backgroundColor: '#8B8B8B',
-                },
+                color: '#00CC22',
                 '& .MuiSlider-thumb': {
-                    height: 20,
-                    width: 20,
                     backgroundColor: '#FFFFFF',
                     border: '1px solid #00CC22',
-                    '&:hover, &.Mui-focusVisible': {
-                        boxShadow: '0px 0px 0px 8px rgba(0, 204, 34, 0.16)',
-                    },
-                    '&.Mui-active': {
-                        boxShadow: '0px 0px 0px 12px rgba(0, 204, 34, 0.16)',
+                    width: 18,
+                    height: 18,
+                    '&:hover': {
+                        boxShadow: '0 0 0 8px rgba(0, 204, 34, 0.16)',
                     },
                 },
+                '& .MuiSlider-track': {
+                    backgroundColor: '#00CC22',
+                    border: 'none',
+                },
+                '& .MuiSlider-rail': {
+                    backgroundColor: '#B7B7B7',
+                    opacity: 1,
+                },
+                '& .MuiSlider-valueLabel': {
+                    backgroundColor: '#00CC22',
+                    borderRadius: 3,
+                },
             }}
-            {...props} // отдаём слайдеру пропсы если они есть (value например там внутри)
+            {...props}
         />
     )
 }
